2eb48de873f8dcf35444d60290ceafe70ed433de
Kamil <alicesun65@gmail.com>
2024-04-02 22:29:48 +0300

    Added character-by-character display of replicas

---
+++ Assets/Scripts/Dialogues/DialogueLine.cs
@@ -0,0 +1,9 @@
+﻿
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueLine
+{
+    public GameObject participant;
+    public string text;
+}
--- Assets/Scripts/Dialogues/Dialogues.cs
+++ Assets/Scripts/Dialogues/Dialogues.cs
@@ -6,26 +6,20 @@ using UnityEngine;
 
 public class Dialogues : MonoBehaviour
 {
-    [System.Serializable]
-    public class DialogueLine
-    {
-        public GameObject participant;
-        public string text;
-    }
-    
     [SerializeField]
-    private List<DialogueLine> _dialogueLines;
-
+    private float _textDisplayingSpeed = 0.1f;
     [SerializeField]
-    private TMP_Text _currentLine;
+    private List<DialogueLine> _dialogueLinesList;
+    private Queue<DialogueLine> _dialogueLines;
+    [SerializeField]
+    private TMP_Text _canvasText;
 
-    private int _dialoguesCount;
-
-    private int curr = 0;
+    private Coroutine _currentCoroutine;
+    private bool _displayFullText = false;
 
     void Awake()
     {
-        _dialoguesCount = _dialogueLines.Count;
+        _dialogueLines = new Queue<DialogueLine>(_dialogueLinesList);
     }
     void Start()
     {
@@ -33,20 +27,38 @@ public class Dialogues : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
         {
 
-            if (_dialoguesCount > 0)
+            if (_dialogueLines.Count > 0)
             {
-                _currentLine.text = _dialogueLines[curr].text;
-                _dialoguesCount--;
-                curr++;
+                if (_currentCoroutine != null)
+                    _displayFullText = true;
+                else
+                {
+                    _canvasText.text = "";
+                    _currentCoroutine = StartCoroutine(DisplayText(_dialogueLines.Dequeue().text));
+                }
+
             }
-            else
-            {
-                Debug.Log("Dialogues End");
-            }
-            Debug.Log(_currentLine.text);
+            else // disable dialogue
+                if (_currentCoroutine == null)
+                    _canvasText.enabled = false;
         }
+    }
+    IEnumerator DisplayText(string text)
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            _canvasText.text += text[i];
+            if (_displayFullText)
+            {
+                _canvasText.text = text;
+                break;
+            }
+            yield return new WaitForSeconds(_textDisplayingSpeed);
+        }
+        _currentCoroutine = null;
+        _displayFullText = false;
     }
 }
