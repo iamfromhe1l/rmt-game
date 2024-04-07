65e988114dbb05b7ff36a81f51ed900bdd22456c
Kamil <alicesun65@gmail.com>
2024-04-07 10:42:10 +0300

    Created ScriptableWizard to generate dialogues

--- Assets/Scripts/Dialogues/DialoguesManager.cs
+++ Assets/Scripts/Dialogues/DialoguesManager.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-namespace DialoguesNamespace
+namespace Dialogues
 {
-    public class Dialogues : MonoBehaviour
+    public class DialoguesManager : MonoBehaviour
     {
         [SerializeField]
         private float _textDisplayingSpeed = 0.1f;
@@ -13,38 +13,40 @@ namespace DialoguesNamespace
         private List<DialogueLine> _dialogueLinesList;
         private Queue<DialogueLine> _dialogueLines;
         [SerializeField]
-        private TMP_Text _canvasText;
-
+        private TMP_Text _dialogueLineText;
+        private TMP_Text _dialogueLineName;
         private Coroutine _currentCoroutine;
-        private bool _displayFullText = false;
+        private bool _displayFullText;
 
+        
+        public void Initialize(float textDisplayingSpeed, List<DialogueLine> dialogueLines)
+        {
+            _textDisplayingSpeed = textDisplayingSpeed;
+            _dialogueLinesList = dialogueLines;
+        }
+        
         void Awake()
         {
             _dialogueLines = new Queue<DialogueLine>(_dialogueLinesList);
-        }
-        void Start()
-        {
-        
+            // get TMP_Text components
         }
         void Update()
         {
             if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
             {
-
                 if (_dialogueLines.Count > 0)
                 {
                     if (_currentCoroutine != null)
                         _displayFullText = true;
                     else
                     {
-                        _canvasText.text = "";
+                        _dialogueLineText.text = "";
                         _currentCoroutine = StartCoroutine(DisplayText(_dialogueLines.Dequeue().text));
                     }
-
                 }
                 else // disable dialogue
                 if (_currentCoroutine == null)
-                    _canvasText.enabled = false;
+                    _dialogueLineText.enabled = false;
             }
         }
         IEnumerator DisplayText(string text)
@@ -52,10 +54,10 @@ namespace DialoguesNamespace
             for (int i = 0; i < text.Length; i++)
             {
                 float randSpace = (text[i] == ' ' ? 1 : 0) * UnityEngine.Random.Range(1, 2) * _textDisplayingSpeed/5;
-                _canvasText.text += text[i];
+                _dialogueLineText.text += text[i];
                 if (_displayFullText)
                 {
-                    _canvasText.text = text;
+                    _dialogueLineText.text = text;
                     break;
                 }
                 yield return new WaitForSeconds(_textDisplayingSpeed + randSpace);
---
+++ Assets/Scripts/Dialogues/GenerateDialogues.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+namespace Dialogues
+{
+    public class GenerateDialog : ScriptableWizard
+    {
+        [SerializeField]
+        private float _textDisplayingSpeed = 0.1f;
+        [SerializeField]
+        private List<DialogueLine> _dialogueLines;
+
+        [MenuItem("GameObject/Generate Dialog")]
+        private static void CreateWizard() => 
+            DisplayWizard<GenerateDialog>("Generate Dialog");
+        
+        private void OnWizardCreate()
+        {
+            GameObject prefab = Resources.Load<GameObject>("[Interface]");
+            GameObject instance = Instantiate(prefab);
+            DialoguesManager dialoguesManager = instance.GetComponentInChildren<DialoguesManager>();
+            dialoguesManager.Initialize(_textDisplayingSpeed, _dialogueLines);
+        }
+    }
+}
