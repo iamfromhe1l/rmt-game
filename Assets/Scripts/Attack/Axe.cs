2a0e413f7f3b9fcc78a505543e23826fa16d1011
DV <149110822+DV-program@users.noreply.github.com>
2024-05-14 15:30:14 +0300

    axe implimentation

---
+++ Assets/Scripts/Attack/Axe.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Interfaces;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.Attack
+{
+    internal class Axe : MonoBehaviour
+    {
+        protected GameObject _attackArea;
+        protected int _damage = 0;
+        protected float _radius = 0;
+        protected Vector3 _center = Vector3.zero;
+        protected float _angle = 0;
+        protected string _tag = string.Empty;
+        private List<IDamageable> Damageables { get; } = new();
+
+        public void StartAttack(int damage)
+        {
+            _damage = damage;
+            StartCoroutine(AxeCoroutine());
+        }
+        public void Init(GameObject prefab, float radius, Vector3 center, float angle, string tagEnemy)
+        {
+            _attackArea = prefab;
+            _radius = radius;
+            _center = center;
+            _angle = angle + 90;
+            _tag = tagEnemy;
+        }
+        private IEnumerator AxeCoroutine()
+        {
+            float startAngle = _angle;
+            while (_angle <= startAngle + 360)
+            {
+                _attackArea.transform.position = new Vector3(
+                    _center.x + _radius * Mathf.Cos(_angle * Mathf.Deg2Rad), 
+                    _attackArea.transform.position.y, 
+                    _center.z + _radius * Mathf.Sin(_angle * Mathf.Deg2Rad));
+                _attackArea.transform.LookAt(_center);
+                _angle += 720 * Time.deltaTime; //Speed
+                yield return null;
+            }
+            Damageables.Clear();
+            Destroy(this.gameObject);
+            yield break;
+        }
+        public void OnTriggerEnter(Collider other)
+        {
+            var damageable = other.GetComponent<IDamageable>();
+            if (damageable != null && other.tag != _tag && !Damageables.Contains(damageable))
+            {
+                Damageables.Add(damageable);
+                damageable.Damage(_damage);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Attack/MeeleAxe.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.Attack;
+using Assets.Scripts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class MeeleAxe : MeeleWeapon
+    {
+        protected UpgradableParametr _enemyMaxCount;
+        [SerializeField] protected GameObject _prefab;
+        [SerializeField] protected Vector3 _offset;
+        AxeCreator axeCreator = new();
+        public override void Attack(int damage)
+        {
+            Axe axe = axeCreator.CreateAttack(
+                _prefab,
+                gameObject.transform.localPosition + Quaternion.Euler(gameObject.transform.eulerAngles) * _offset,
+                gameObject.transform.eulerAngles,
+                _offset.z,
+                gameObject.transform.position + new Vector3(0,_offset.y,0),
+                gameObject.transform.eulerAngles.y,
+                tag
+                );
+            axe.StartAttack(damage);
+        }
+
+        public override UpgradableParametr Upgrade(string lvl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
