5ddfc1801f9ebda74e5442556e470a9f81a9379f
iamfromhe1l <sgadgsfad@gmail.com>
2024-05-23 05:29:26 +0300

    config fix

--- Assets/Scripts/Configs/WeaponConfig.cs
+++ Assets/Scripts/Configs/WeaponConfig.cs
@@ -8,31 +8,31 @@ public class WeaponConfig
     public static Dictionary<string, Dictionary<int, int>> swordLevels = new Dictionary<string, Dictionary<int, int>>()
     {
         {"damage", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"distance", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"timeout", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
+        {"distance", new Dictionary<int, int>() { { 0, 1 }, { 1, 2 }, { 2, 3 } }},
+        {"timeout", new Dictionary<int, int>() { { 0, 3 }, { 1, 2 }, { 2, 1 } }},
     };
 
     public static Dictionary<string, Dictionary<int, int>> axeLevels = new Dictionary<string, Dictionary<int, int>>()
     {
-        {"damage", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"distance", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"timeout", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"enemyMaxCount", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
+        {"damage", new Dictionary<int, int>() { { 0, 7 }, { 1, 14 }, { 2, 21 } }},
+        {"distance", new Dictionary<int, int>() { { 0, 1 }, { 1, 2 }, { 2, 3 } }},
+        {"timeout", new Dictionary<int, int>() { { 0, 5 }, { 1, 4 }, { 2, 3 } }},
+        {"enemyMaxCount", new Dictionary<int, int>() { { 0, 2 }, { 1, 3 }, { 2, 4 } }},
     };
 
     public static Dictionary<string, Dictionary<int, int>> fireLevels = new Dictionary<string, Dictionary<int, int>>()
     {
-        {"damage", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"speed", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"timeout", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"count", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
+        {"damage", new Dictionary<int, int>() { { 0, 15 }, { 1, 25 }, { 2, 35 } }},
+        {"speed", new Dictionary<int, int>() { { 0, 5 }, { 1, 10 }, { 2, 15 } }},
+        {"timeout", new Dictionary<int, int>() { { 0, 5 }, { 1, 4 }, { 2, 3 } }},
+        {"count", new Dictionary<int, int>() { { 0, 1 }, { 1, 2 }, { 2, 3 } }},
     };
 
     public static Dictionary<string, Dictionary<int, int>> windLevels = new Dictionary<string, Dictionary<int, int>>()
     {
-        {"damage", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"speed", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"timeout", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
-        {"reclining", new Dictionary<int, int>() { { 0, 10 }, { 1, 20 }, { 2, 30 } }},
+        {"damage", new Dictionary<int, int>() { { 0, 15 }, { 1, 25 }, { 2, 35 } }},
+        {"speed", new Dictionary<int, int>() { { 0, 5 }, { 1, 10 }, { 2, 15 } }},
+        {"timeout", new Dictionary<int, int>() { { 0, 5 }, { 1, 4 }, { 2, 3 } }},
+        {"reclining", new Dictionary<int, int>() { { 0, 5 }, { 1, 10 }, { 2, 15 } }},
     };
 }
