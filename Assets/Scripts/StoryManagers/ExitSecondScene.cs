65349cc291fe9d8c45b153d2a3db4c632fb797c6
Kamil <alicesun65@gmail.com>
2024-05-23 09:24:04 +0300

    Finished 2 scene story

---
+++ Assets/Scripts/StoryManagers/ExitSecondScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ScenesManager
+{
+    public class ExitSecondScene : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("HeroTag"))
+            {
+                FindObjectOfType<ScreenFader>().FadeToBlack();
+                Invoke("nextScene",3f);
+            }
+        }
+
+        private void nextScene()
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+}
